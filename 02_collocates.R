51ce9792000771ef3404387368abaddb623658cd
rll307 <rll307@unicamp.br>
2023-07-27 09:27:13 -0300

    Updated Collocates

--- 02_collocates.R
+++ 02_collocates.R
@@ -54,19 +54,208 @@ BasicStats <-
     Tweets = length(Moise.t$tweet_id)
   ) 
 
-source("._FunctionCollocates.R")
+#Analisis -----------------------------------------------------------------
+#Refurgiado
 
 coocTerm <- "refugiado"
 
-Refugiado <-
-  calculateCoocStatistics(coocTerm,
-                          Moise.d,
-                          measure = "LOGLIK") |> 
+Refugiado <- calculateCoocStatistics(coocTerm,
+                                     Moise.d,
+                                     measure = "LOGLIK") |>
   data.frame()
+
 colnames(Refugiado)[1] <- "Value"
 Refugiado$Word <- rownames(Refugiado)
+#Refugiado$n <- 1:length(Refugiado$Value)
 Refugiado$Value <- round(Refugiado$Value, 2)
 Refugiado$from <- "refugiado"
+
+# Moïse
+coocTerm <- "moise"
+MoiseC <- calculateCoocStatistics(coocTerm,
+                                  Moise.d,
+                                  measure = "LOGLIK") |>
+  data.frame()
+
+
+colnames(MoiseC)[1] <- "Value"
+MoiseC$Word <- rownames(MoiseC)
+#MoiseC$n <- 1:length(MoiseC$Value)
+MoiseC$Value <- round(MoiseC$Value, 2)
+MoiseC$from <- "moise"
+
+#MoiseC |> View()
+# imigrante
+
+coocTerm <- "imigrante"
+imigrante <- calculateCoocStatistics(coocTerm,
+                                     Moise.d,
+                                     measure = "LOGLIK") |>
+  data.frame()
+
+colnames(imigrante)[1] <- "Value"
+imigrante$Word <- rownames(imigrante)
+#imigrante$n <- 1:length(imigrante$Value)
+imigrante$Value <- round(imigrante$Value, 2)
+imigrante$from <- "imigrante"
+#imigrante |> View()
+
+# congoles
+
+coocTerm <- "congoles"
+congoles <- calculateCoocStatistics(coocTerm,
+                                    Moise.d,
+                                    measure = "LOGLIK") |>
+  data.frame()
+
+colnames(congoles)[1] <- "Value"
+congoles$Word <- rownames(congoles)
+#congoles$n <- 1:length(congoles$Value)
+congoles$Value <- round(congoles$Value, 2)
+congoles$from <- "congoles"
+#congoles |> View()
+
+# estrangeiro
+
+coocTerm <- "estrangeiro"
+estrangeiro <- calculateCoocStatistics(coocTerm,
+                                       Moise.d,
+                                       measure = "LOGLIK") |>
+  data.frame()
+
+colnames(estrangeiro)[1] <- "Value"
+estrangeiro$Word <- rownames(estrangeiro)
+#estrangeiro$n <- 1:length(estrangeiro$Value)
+estrangeiro$Value <- round(estrangeiro$Value, 2)
+estrangeiro$from <- "estrangeiro"
+#colnames(estrangeiro)[2] <- "to"
+#names <- rownames(estrangeiro)
+
+#estrangeiro |> View()
+
+# negro
+
+coocTerm <- "negro"
+negro <- calculateCoocStatistics(coocTerm,
+                                 Moise.d,
+                                 measure = "LOGLIK") |>
+  data.frame()
+
+colnames(negro)[1] <- "Value"
+negro$Word <- rownames(negro)
+#negro$n <- 1:length(negro$Value)
+negro$Value <- round(negro$Value, 2)
+negro$from <- "negro"
+#colnames(negro)[2] <- "to"
+#names <- rownames(negro)
+#negro |> View()
+
+# Preparing Matrices ------------------------------------------------------
+Negro_M <- negro[1:10,] #A
+Refugiado_M <- Refugiado[1:10,] #B
+MoiseC_M <- MoiseC[1:10,] #C
+Congoles_M <- congoles[1:10,] #D
+Estrangeiro_M <- estrangeiro[1:10,] #E
+Imigrante_M <- imigrante[1:10,] #F1
+
+
+# Plotting ----------------------------------------------------------------
+
+set.seed(5645)  
+colors2 <- colorRampPalette(c("lightgray","gray", "darkgray", "black"))(10)
+
+A <- Negro_M %>%
+  mutate(Word = fct_reorder(Word, Value)) %>%
+  ggplot(aes(
+    y = Word,
+    x = from,
+    fill = Value)) +
+  geom_tile() +
+  scale_fill_gradientn(colours = colors2) +
+  coord_equal() +
+  labs(
+    x = "Nódulo",
+    y = "Colocados",
+    fill = "Verosimilhança (log)"
+  )
+
+B <- Refugiado_M %>%
+  mutate(Word = fct_reorder(Word, Value)) %>%
+  ggplot(aes(
+    y = Word,
+    x = from,
+    fill = Value)) +
+  geom_tile() +
+  scale_fill_gradientn(colours = colors2) +
+  coord_equal() +
+  labs(
+    x = "Nódulo",
+    y = "Colocados",
+    fill = "Verosimilhança (log)"
+  )
+
+C <- MoiseC_M %>%
+  mutate(Word = fct_reorder(Word, Value)) %>%
+  ggplot(aes(
+    y = Word,
+    x = from,
+    fill = Value)) +
+  geom_tile() +
+  scale_fill_gradientn(colours = colors2) +
+  coord_equal() +
+  labs(
+    x = "Nódulo",
+    y = "Colocados",
+    fill = "Verosimilhança (log)"
+  )
+
+D <- Congoles_M %>%
+  mutate(Word = fct_reorder(Word, Value)) %>%
+  ggplot(aes(
+    y = Word,
+    x = from,
+    fill = Value)) +
+  geom_tile() +
+  scale_fill_gradientn(colours = colors2) +
+  coord_equal() +
+  labs(
+    x = "Nódulo",
+    y = "Colocados",
+    fill = "Verosimilhança (log)"
+  )
+
+E <- Estrangeiro_M %>%
+  mutate(Word = fct_reorder(Word, Value)) %>%
+  ggplot(aes(
+    y = Word,
+    x = from,
+    fill = Value)) +
+  geom_tile() +
+  scale_fill_gradientn(colours = colors2) +
+  coord_equal() +
+  labs(
+    x = "Nódulo",
+    y = "Colocados",
+    fill = "Verosimilhança (log)"
+  )
+
+F1 <- Imigrante_M %>%
+  mutate(Word = fct_reorder(Word, Value)) %>%
+  ggplot(aes(
+    y = Word,
+    x = from,
+    fill = Value)) +
+  geom_tile() +
+  scale_fill_gradientn(colours = colors2) +
+  coord_equal() +
+  labs(
+    x = "Nódulo",
+    y = "Colocados",
+    fill = "Verosimilhança (log)"
+  )
+
+
+grid.arrange(A, B, C,D,E,F1, ncol = 3) 
 
 
 
